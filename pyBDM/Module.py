e830c56307937b12dbba55e5f315c12c71fce037
Christoph Schueler <cpu12.gems@googlemail.com>
2013-06-30 10:58:54 +0200

    Added 'Module.py'.

---
+++ pyBDM/Module.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__version__ = '0.1.0'
+
+__copyright__ = \
+    """
+    pyBDM - Library for the Motorola/Freescale Background Debugging Mode.
+
+   (C) 2010-2013 by Christoph Schueler <github.com/Christoph2,
+                                        cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
+
+__all__ = ['Module']
+
+import logging
+
+logger = logging.getLogger('pyBDM')
+
+def registerProperty(name, registerAddress, eightBit = True, doc = None):
+    def fget8( self):
+        return self.read8(registerAddress)
+
+    def fset8(self, value):
+        self.write8(registerAddress, value)
+
+    def fget16( self):
+        return self.read16(registerAddress)
+
+    def fset16(self, value):
+        self.write16(registerAddress, value)
+
+    if eightBit:
+        return property(fget = fget8, fset = fset8, doc = doc)
+    else:
+        return property(fget = fget16, fset = fset16, doc = doc)
+
+
+class Module(object):
+    def __init__(self, port):
+        self._port = port
+        if hasattr(self, '__REGISTERS8__'):
+            for name, reg in self.__REGISTERS8__:
+                setattr(self.__class__, name, registerProperty(name, reg, True))
+        if hasattr(self, '__REGISTERS16__'):
+            for name, reg in self.__REGISTERS16__:
+                setattr(self.__class__, name, registerProperty(name, reg, False))
+
+    def read8(self, addr):
+        return self.port.readByte(addr)
+
+    def write8(self, addr, value):
+        self.port.writeByte(addr, value)
+
+    def read16(self, addr):
+        return self.port.readWord(addr)
+
+    def write16(self, addr, value):
+        self.port.writeWord(addr, value)
+
+    def _getPort(self):
+        return self._port
+
+    port = property(_getPort)
+
--- setup.py
+++ setup.py
@@ -10,16 +10,15 @@ setup(
     author_email='cpu12.gems@googlemail.com',
     url='http://www.github.com/Christoph2/pyBDM',
     packages=['pyBDM', 'PyBDM/S12'],
-#    entry_points = {
-#	'console_scripts': [
+    entry_points = {
+	'console_scripts': [
+		'bdm_partinfo = pyBDM.scripts.bdm_partinfo:main'
 #	    'foo = my_package.some_module:main_func',
 #	    'bar = other_module:some_func',
-#        ],
-#    }
-
-    install_requires=['pyserial', 'puremvc'
+        ],
+    },
+    install_requires=[
+	'pyserial', 'pyusb', #'wxpython'
     ],
-#    scripts=[
-#    ]
 )
 
