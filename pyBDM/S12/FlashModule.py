f805915933550a6c6f7a234fdcb8bf6d7159ed7d
Christoph Schueler <cpu12.gems@googlemail.com>
2014-01-31 08:01:05 +0100

    [FlashModule.py] Rework Flash class.

--- pyBDM/S12/FlashModule.py
+++ pyBDM/S12/FlashModule.py
@@ -1,8 +1,38 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+__version__ = '0.1.0'
+
+__copyright__ = \
+    """
+    pyBDM - Library for the Motorola/Freescale Background Debugging Mode.
+
+   (C) 2010-2013 by Christoph Schueler <github.com/Christoph2,
+                                        cpu12.gems@googlemail.com>
+
+   All Rights Reserved
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+"""
 
 from pyBDM.Module import Module
 
+
+##
+##  Flash Registers.
+##
 FCLKDIV             =     0x0100
 # FCLKDIV-Bits.
 FDIVLD      =     0x80
@@ -73,6 +103,22 @@ CMDB0       =     0x01
 FADDR               =     0x0108
 FDATA               =     0x011a
 
+"""
+$FF0D Flash 0
+$FF0C Flash 1
+$FF0B Flash 2
+$FF0A Flash 3
+"""
+
+##
+##  Flash Commands.
+##
+ERASE_VERIFY    = 0x05
+PROGRAM         = 0x20
+SECTOR_ERASE    = 0x40
+MASS_ERASE      = 0x41
+
+
 class Flash(Module):
     __REGISTERS8__ = (
         ('fclkdiv', FCLKDIV),
@@ -82,9 +128,22 @@ class Flash(Module):
         ('fprot',   FPROT),
         ('fstat',   FSTAT),
         ('fcmd',    FCMD),
+    )
+
+    __REGISTERS16__ = (
         ('faddr',   FADDR),
         ('fdata',   FDATA),
     )
+
+    LRS_512     = 0x00
+    LRS_1K      = 0x01
+    LRS_2K      = 0x02
+    LRS_4K      = 0x03
+
+    HRS_2K      = 0x00
+    HRS_4K      = 0x01
+    HRS_8K      = 0x02
+    HRS_16K     = 0x03
 
     def selectBank(self, bank):
         tmp = self.fcnfg & (CBEIE | CCIE | KEYACC)
@@ -102,4 +161,153 @@ class Flash(Module):
     def keyAccess(self):
         return (self.fsec & 0xc0) == 0x80
 
+    def errors(self):
+        return self.fstat & (PVIOL | ACCERR)
 
+    # protectAll()
+    def protect(self, lower, lowerRangeSize, upper, upperRangeSize):
+        tmp = self.fprot
+
+        fphdis = 0x00 if upper else FPHDIS
+        fpldis = 0x00 if lower else FPLDIS
+
+        #tmp = ()
+        self.fprot = tmp
+
+        self._flashCommand(PROGRAM, 0xFF0E, 0xFF00)
+
+    def unprotect(self, lower, upper):
+        tmp = self.fprot
+
+        self.fprot = tmp
+
+    def _waitReady(self):
+        while (self.fstat & CBEIF) == 0x00:
+            pass    ## TODO: timeout!
+
+    def _waitCompletion(self):
+        while (self.fstat & CCIF) == 0x00:
+            pass    ## TODO: timeout!
+
+    def _flashCommand(self, command, addr, data):
+        print "FCnfg: %#x" % self.fcnfg
+        print "Fprot: %#x" % self.fprot
+        print "Fstat: %#x %#x" % (self.fstat, self.errors())
+        self._waitReady()
+        ## TODO: Check for Address alignment!!!
+        self._port.writeWord(addr, data)
+
+        print "Fstat: %#x %#x" % (self.fstat, self.errors())
+        self.fcmd = command
+        print "Fstat: %#x %#x" % (self.fstat, self.errors())
+        self.fstat = CBEIF  # Start command.
+        print "Fstat: %#x %#x" % (self.fstat, self.errors())
+        errors = self.errors()
+        if errors:
+            # TODO: Vernünftiges Errorhandling!!!
+            print "Errors while flashing: %#x" % errors
+        else:
+            print "Fstat: %#x %#x" % (self.fstat, self.errors())
+            self._waitCompletion()
+
+    def eraseBlock(self, block):
+        self._flashCommand(MASS_ERASE, 0x8000, 0xaffe)
+
+    def eraseAll(self):
+        self.writeAll()
+        self.fcnfg = 0x00
+        self.fprot = FPOPEN | FPHDIS | FPLDIS
+        self._flashCommand(MASS_ERASE, 0xc000, 0xaffe)
+
+
+    def test(self, addr, data):
+        self._flashCommand(PROGRAM, addr, data)
+
+    def writeAll(self):
+        self.ftstmod = WRALL
+
+    #def protect(self):
+    #    self.fprot = 0x00
+
+    def unsecure(self):
+        self.eraseAll()
+        return
+
+        self.fcnfg = 0x00
+        self.writeAll()
+        self.fprot = FPOPEN | FPHDIS | FPLDIS
+        self._port.writeWord(0xc000, 0xaffe)
+        self.fcmd = MASS_ERASE
+        self.fstat = CBEIF
+        while (self.fstat & CCIF) == 0x00:
+            pass
+
+    def lockUnsecure(self):
+        """
+            a. Write FCLKDIV register to set the Flash clock for proper timing.
+            b. Write $00 to FCNFG register to select Flash block 0.
+            c. Disable Flash protection by writing the FPROT register.
+            d. Write $FFFE to address $FF0E
+            e. Write Program command($20) to FCMD register.
+            f. Clear CBIEF (bit 7) it FSTAT register.
+            g. Wait until Flash CCIF flag is set to 1 again.
+        """
+        self.fcnfg = 0x00
+        self.fprot = FPOPEN | FPHDIS | FPLDIS
+        self._port.writeWord(0xFF0E, 0xFFFE)
+        self.fcmd = PROGRAM
+        self.fstat = CBEIF
+        while (self.fstat & CCIF) == 0x00:
+            pass
+        print "Finished."
+
+"""
+FTS256:
+4 Block a 4 Pages
+
+Block #     Page
+================
+0           $3c
+            $3d
+            $3e
+            $3f
+
+1           $38
+            $39
+            $3a
+            $3b
+
+2           $34
+            $35
+            $36
+            $37
+
+3           $30
+            $31
+            $32
+            $33
+"""
+
+"""
+
+static void S12Fls_PageSelect(uint8 page)
+{
+    S12FLS_REG8(FCNFG)    &= ~(FLS_NUM_BANKS - 1);
+    S12FLS_REG8(FCNFG)    |= ((page - FLS_PPAGE_OFFSET) >> 2) ^ (FLS_NUM_BANKS - 1);
+    S12MMC_REG8(PPAGE)     = page;
+}
+
+
+static void S12Fls_ClearPendingErrors(void)
+{
+    uint8 idx;
+
+    for (idx = (uint8)0x00; idx < FLS_NUM_BANKS; ++idx) {
+        S12FLS_REG8(FCNFG)    &= ~(FLS_NUM_BANKS - 1);
+        S12FLS_REG8(FCNFG)    |= idx;
+        S12FLS_REG8(FSTAT)     = (PVIOL | ACCERR);
+    }
+}
+
+
+"""
